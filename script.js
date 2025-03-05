35681b6a0ac77789986b2abcee349a691f3914dd
susyfs <syysusilva@gmail.com>
2025-03-05 16:39:22 -0400

    add correçoes  html

--- script.js
+++ script.js
@@ -1,52 +1,61 @@
 $(document).ready(function() {
-    $('mobile_btn').on('click', function(){}
-    $('mobile_btn').toggeClass('active');
-    $('mobile_btn').find('i').toggleClass('fa-x');
-});
-const sections = $('section');
-const naItems = $ (' nav-item');
-
-$(window).on('scroll',function(){
-    const header = $ ('header');
-    const scrollPosition = $ ( whindow).scrolltop() - header. outerHeight();
-
-    let activeSectionIndex = 0;
-
-    if(scrollPosition<= 0) {
-        header.css('box-shadow', 'none');
-    } else{
-        header.css('box-shadow', '5px 1px 5px) rgba(0 ,0 ,0 , 0.1)');
-    }
-    sections.each(function(i) {
-        const section = $(this);
-        const sectionTop = section.offset().top - 96;
-        const sectionBottom = sectionTop + section.outerHeight();
-
-        if(scrollPosition >= sectionTop && scrollPosition < sectionBottom){
-            activeSectionIndex = i ;
-            return false
+    $('#mobile_btn').on('click', function () {
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggleClass('fa-x');
+    });
+  
+    const sections = $('section');
+    const navItems = $('.nav-item');
+  
+    $(window).on('scroll', function () { 
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+        let activeSectionIndex = 0;
+  
+        if (scrollPosition <= 0) {
+            header.css('box-shadow', 'none');
+        } else {
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
         }
+  
+        sections.each(function(i) {
+            const section = $(this);
+            const sectionTop = section.offset().top - 96; 
+            const sectionBottom = sectionTop+ section.outerHeight();
+  
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                activeSectionIndex = i;
+                return false;
+            }
+        })
+  
+        navItems.removeClass('active');
+        $(navItems[activeSectionIndex]).addClass('active');
     });
-    naItems.removeClass('active');
-    $(naItems[activeSectionIndex]).addClass('active');
-});
-
-    /**animação */
-    ScrollReaveal().reaveal('#cta',{
-        origin: 'left',
-        duration: 2000,
-        distance: '20%'
-    });
+  
     
-    ScrollReaveal().reaveal('#.burguer',{
+    /** Animação */
+    ScrollReveal().reveal('#cta', {
         origin: 'left',
         duration: 2000,
         distance: '20%'
     });
-
-    ScrollReaveal().reaveal('#testimonial_burguer',{
+  
+    ScrollReveal().reveal('.burguer', {
         origin: 'left',
         duration: 2000,
         distance: '20%'
     });
-});
+  
+    ScrollReveal().reveal('#testimonial_burguer', {
+        origin: 'left',
+        duration: 1000,
+        distance: '20%'
+    })
+  
+    ScrollReveal().reveal('.feedback', {
+        origin: 'right',
+        duration: 1000,
+        distance: '20%'
+    })
+  });
